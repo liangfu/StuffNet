385e01274a1c676185f177aa97a0d586ec9f9a83
Liangfu Chen <liangfu.chen@harman.com>
2017-11-16 14:07:37 +0800

    update default input image size to 1024x512;

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -35,10 +35,10 @@ __C.TRAIN = edict()
 
 # Scales to use during training (can list multiple scales)
 # Each scale is the pixel size of an image's shortest side
-__C.TRAIN.SCALES = (600,)
+__C.TRAIN.SCALES = (512,)
 
 # Max pixel size of the longest side of a scaled input image
-__C.TRAIN.MAX_SIZE = 1000
+__C.TRAIN.MAX_SIZE = 1024
 
 # Images to use per minibatch
 __C.TRAIN.IMS_PER_BATCH = 2
@@ -133,10 +133,10 @@ __C.TEST = edict()
 
 # Scales to use during testing (can list multiple scales)
 # Each scale is the pixel size of an image's shortest side
-__C.TEST.SCALES = (600,)
+__C.TEST.SCALES = (512,)
 
 # Max pixel size of the longest side of a scaled input image
-__C.TEST.MAX_SIZE = 1000
+__C.TEST.MAX_SIZE = 1024
 
 # Overlap threshold used for non-maximum suppression (suppress boxes with
 # IoU >= this threshold)
