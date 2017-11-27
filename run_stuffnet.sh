d301c79faf2d09913978d22f2362338eb3a942a6
Liangfu Chen <liangfu.chen@harman.com>
2017-11-27 20:13:55 +0800

    adapt cityscapes dataset;

--- run_stuffnet.sh
+++ run_stuffnet.sh
@@ -1,24 +1,15 @@
 #!/usr/bin/env bash
 
 # ./experiments/scripts/faster_rcnn_end2end-seg.sh 0 vgg16 pascal_voc --vis
-
-# ./tools/test_net.py --gpu 0 --def models/pascal_voc/vgg16/faster_rcnn_end2end/test-seg.prototxt --net output/faster_rcnn_end2end/voc_2010_train/vgg16_faster_rcnn-seg_iter_70000.caffemodel --imdb voc_2010_val --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml --comp --vis
-
-# python -u ./tools/train_net.py --gpu 0 --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml \
-#        --solver models/pascal_voc/vgg16/faster_rcnn_end2end/solver-seg.prototxt \
-#        --weights models/imagenet/VGG_ILSVRC_16_layers.caffemodel
-
 set -v
-
 export PYTHONPATH=/home/liangfu/local/python:$PYTHONPATH
 export GLOG_minloglevel=0
 
-python -u ./tools/train_net.py --gpu 0 --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml \
-       --solver models/cityscapes/vgg16/faster_rcnn_end2end/solver-seg.prototxt \
-       --weights models/imagenet/VGG_ILSVRC_16_layers.caffemodel \
-       --imdb cityscapes_train
+# python -u ./tools/test_net.py --gpu 0 --def models/pascal_voc/vgg16/faster_rcnn_end2end/test-seg.prototxt --net output/faster_rcnn_end2end/voc_2010_train/vgg16_faster_rcnn-seg_iter_70000.caffemodel --imdb voc_2010_val --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml --comp --vis
+
+# python -u ./tools/test_net.py --gpu 0 --def models/cityscapes/vgg16/faster_rcnn_end2end/test-seg.prototxt --net output/faster_rcnn_end2end/cityscapes_2017_train/vgg16_faster_rcnn-seg_iter_20000.caffemodel --imdb cityscapes_val --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml --comp --vis
+
+python -u ./tools/train_net.py --gpu 0 --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml --solver models/cityscapes/vgg16/faster_rcnn_end2end/solver-seg.prototxt --weights models/imagenet/VGG_ILSVRC_16_layers.caffemodel --imdb cityscapes_train
 
 set +v
-
-# pdb tools/train_net.py --gpu 0 --cfg experiments/cfgs/faster_rcnn_end2end-seg.yml --solver models/cityscapes/vgg16/faster_rcnn_end2end/solver-seg.prototxt --weights models/imagenet/VGG_ILSVRC_16_layers.caffemodel --imdb cityscapes_train
 
